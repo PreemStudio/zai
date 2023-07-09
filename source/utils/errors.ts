db95e2bb52d3f163c3824d14e080f0a69053ba42
Brian Faust <hello@basecode.sh>
2023-07-09 07:10:17 +0300

    MAINTENANCE: Update GitHub links

--- source/utils/errors.ts
+++ source/utils/errors.ts
@@ -1,28 +1,25 @@
-import chalk from 'chalk';
+import chalk from "chalk";
 
-const APPLICATION_VERSION = '1.0.0';
+const APPLICATION_VERSION = "1.0.0";
 
 export class ApplicationError extends Error {}
 
-const indent = '    ';
+const indent = "    ";
 
 export function handleCliError(error: any) {
-	if (
-		error instanceof Error
-		&& !(error instanceof ApplicationError)
-	) {
+	if (error instanceof Error && !(error instanceof ApplicationError)) {
 		if (error.stack) {
 			console.error(
-				chalk.dim(error.stack.split('\n').slice(1).join('\n')),
+				chalk.dim(error.stack.split("\n").slice(1).join("\n"))
 			);
 		}
 
 		console.error(`\n${indent}${chalk.dim(`zai v${APPLICATION_VERSION}`)}`);
 		console.error(
-			`\n${indent}Please open a Bug report with the information above:`,
+			`\n${indent}Please open a Bug report with the information above:`
 		);
 		console.error(
-			`${indent}https://github.com/BombenProdukt/zai/issues/new/choose`,
+			`${indent}https://github.com/faustbrian/zai/issues/new/choose`
 		);
 	}
 }
